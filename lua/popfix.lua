32db19424239af88f51795aee550b8301d9b7ec9
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2020-09-14 17:31:17 +0530

    Write way to init plugin

--- lua/popfix.lua
+++ lua/popfix.lua
@@ -1,17 +1,17 @@
 local api = vim.api
+local function open_window()
+	local buf = api.nvim_create_buf(false, true)
 
-local function openWindow(buf, win)
-	buf = api.nvim_create_buf(false,true)
-	api.nvim_buf_set_option(buf,'bufhidden','wipe')
+	api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
 
 	local width = api.nvim_get_option("columns")
 	local height = api.nvim_get_option("lines")
 
-	local win_height = math.ceil((height * 0.8 -4))
+	local win_height = math.ceil(height * 0.8 - 4)
 	local win_width = math.ceil(width * 0.8)
 
-	local row = math.ceil((height - win_height) /2 -1)
-	local col = math.ceil((width - win_width) /2)
+	local row = math.ceil((height - win_height) / 2 - 1)
+	local col = math.ceil((width - win_width) / 2)
 
 	local opts = {
 		style = "minimal",
@@ -22,11 +22,19 @@ local function openWindow(buf, win)
 		col = col
 	}
 
-	win = api.nvim_open_win(buf,true,opts)
+	-- and finally create it with buffer attached
+	local win = api.nvim_open_win(buf, true, opts)
+	local ret = {}
+	ret[1] = buf;
+	ret[2] = win;
+	return ret
 end
 
-function popup_window()
-	local buf, win
-	openWindow(buf,win)
-	print(win)
+local function popup_window()
+	local newWindow = open_window()
+	print(newWindow[2])
 end
+
+return{
+	popup_window = popup_window
+}
--- plugin/popfix.vim
+++ plugin/popfix.vim
@@ -1,11 +1,8 @@
-if exists('g:loaded_popfix') | finish | endif " prevent loading file twice
+fun! PopFix()
+    lua for k in pairs(package.loaded) do if k:match("^popfix") then package.loaded[k] = nil end end
+	lua require'popfix'.popup_window()
+endfun
 
-let s:save_cpo = &cpo " save user coptions
-set cpo&vim " reset them to defaults
-
-let &cpo = s:save_cpo " and restore after
-unlet s:save_cpo
-
-command !Popup lua require'popfix'.popup_window()
-
-let g:loaded_popfix = 1
+augroup PopFix
+    autocmd!
+augroup END
