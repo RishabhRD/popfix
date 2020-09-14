6b787f1b0676a8d4609b5b32ea94198cd967bf4c
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2020-09-14 19:13:10 +0530

    popup window now shows string

--- lua/popfix.lua
+++ lua/popfix.lua
@@ -1,28 +1,52 @@
 local api = vim.api
-local function open_window()
+
+local function open_window(data,maxWidth,maxHeight)
+	local minWidth = 30
+	local minHeight = 5
+	if #data == 0 then
+		return nil
+	end
+	if maxHeight < minHeight then
+		return nil
+	end
+	if maxWidth < minWidth then
+		return nil
+	end
 	local buf = api.nvim_create_buf(false, true)
-
 	api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
+	api.nvim_buf_set_lines(buf,0,-1,false,data)
+	api.nvim_buf_set_option(buf, 'modifiable',false)
+	-- local width = api.nvim_get_option("columns")
+	-- local height = api.nvim_get_option("lines")
 
-	local width = api.nvim_get_option("columns")
-	local height = api.nvim_get_option("lines")
+	local winHeight = #data + 1
+	if winHeight < minHeight then
+		winHeight = minHeight
+	end
+	if winHeight > maxHeight then
+		winHeight = maxHeight
+	end
 
-	local win_height = math.ceil(height * 0.8 - 4)
-	local win_width = math.ceil(width * 0.8)
-
-	local row = math.ceil((height - win_height) / 2 - 1)
-	local col = math.ceil((width - win_width) / 2)
+	local winWidth = minWidth
+	for _,cur in pairs(data) do
+		local curWidth =  string.len(cur)
+		if curWidth > winWidth then
+			winWidth = curWidth
+		end
+	end
+	if winWidth > maxWidth then
+		winWidth = maxWidth
+	end
 
 	local opts = {
 		style = "minimal",
-		relative = "editor",
-		width = win_width,
-		height = win_height,
-		row = row,
-		col = col
+		relative = "cursor",
+		width = winWidth,
+		height = winHeight,
+		row = 0,
+		col = 1
 	}
 
-	-- and finally create it with buffer attached
 	local win = api.nvim_open_win(buf, true, opts)
 	local ret = {}
 	ret[1] = buf;
@@ -30,8 +54,8 @@ local function open_window()
 	return ret
 end
 
-local function popup_window()
-	local newWindow = open_window()
+local function popup_window(data,maxWidth,maxHeight)
+	local newWindow = open_window(data,maxWidth,maxHeight)
 	print(newWindow[2])
 end
 
